ef67021ec2fb6b477d8db960c1865f2804285cbb
Prithvish Baidya <deformercoding@gmail.com>
2022-05-17 19:43:52 +0530

    Refactored code

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -2,106 +2,129 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TimeLock is Ownable {
-    using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    enum depositType {
+    enum DepositType {
         ETH,
         ERC20
     }
 
-    struct lockedToken {
+    struct Deposit {
         IERC20 token;
-        address payable withdrawer;
+        address payable receiver;
+        address sender;
         uint256 amount;
         uint256 unlockTimeStamp;
-        bool  withdrawn;
-        bool deposited;
+        bool withdrawn;
     }
 
-    uint256 public depositsCount;
-    mapping (address => uint256[]) public depositsByTokenAddress;
-    mapping (address => uint256[]) public depositsByWithdrawers;
-    mapping (uint256 => lockedToken) public vault;
+    mapping(address => uint256) public senderDepositCount;
+    mapping(address => uint256) public receiverDepositCount;
 
-    // userAdress -> tokenAddress -> token amount
-    mapping (address => mapping (address => uint256)) public userTokenBalance;
+    Deposit[] public deposits;
 
-    event tokenDepositComplete(address token, uint256 amount);
-    event tokenWithdrawalComplete(address withdrawer, uint256 amount);
+    event TokenDeposit(uint256 depositId);
+    event TokenWithdrawal(uint256 depositId);
 
     // function depositAndLockETH(
-    //     address payable _withdrawer,
-    //     uint256 _amount,
-    //     uint256 _unlockTimeStamp
-    //     ) external payable returns (uint256) {
-            
-    //         require(msg.value > 0, "No amount mentioned");
-    //         require(_unlockTimeStamp > block.timestamp, "Unlock timestamp is not in the future!");
+    // address payable _withdrawer,
+    // uint256 _amount,
+    // uint256 _unlockTimeStamp
+    // ) external payable returns (uint256) {
+    // require(msg.value > 0, "No amount mentioned");
+    // require(_unlockTimeStamp > block.timestamp, "Unlock timestamp is not in the future!");
+    // }
 
-
-    //         }
-
-    function depositAndLockTokens(
+    function depositERC20(
         IERC20 _token,
-        address payable _withdrawer, 
-        uint256 _amount, 
+        address payable _receiver,
+        uint256 _amount,
         uint256 _unlockTimeStamp
-        )
-            external returns (uint256 _id) {
+    ) external returns (uint256 _id) {
+        require(
+            _unlockTimeStamp > block.timestamp,
+            "ERR__INVALID_TIME"
+        );
+        require(
+            _token.allowance(msg.sender, address(this)) >= _amount,
+            "ERR__NOT_ENOUGH_ALLOWANCE"
+        );
 
-                require(_unlockTimeStamp > block.timestamp, "Unlock timestamp is not in the future!");
-                require(_token.allowance(msg.sender, address(this)) >= _amount, "You should have more tokens than you wish to deposit");
+        _token.transferFrom(msg.sender, address(this), _amount);
 
-                _token.transferFrom(msg.sender, address(this), _amount);
+        Deposit memory newDeposit = Deposit(
+            _token,
+            _receiver,
+            msg.sender,
+            _amount,
+            _unlockTimeStamp,
+            false
+        );
 
-                userTokenBalance[msg.sender][address(_token)] += _amount;
+        deposits.push(newDeposit);
+        uint256 id = deposits.length - 1;
 
-                
-                _id = ++depositsCount;
-                vault[_id].token = _token;
-                vault[_id].withdrawer = _withdrawer;
-                vault[_id].amount = _amount;
-                vault[_id].unlockTimeStamp = _unlockTimeStamp;
-                vault[_id].withdrawn = false;
-                vault[_id].deposited = true;
+        senderDepositCount[msg.sender]++;
+        receiverDepositCount[_receiver]++;
 
-                depositsByTokenAddress[address(_token)].push(_id);
-                depositsByWithdrawers[_withdrawer].push(_id);
-
-                emit tokenDepositComplete(address(_token), _amount);
-
-                return _id;
-            }
+        emit TokenDeposit(id);
+        return id;
+    }
 
     function withdrawTokens(uint256 _id) external {
+        Deposit storage myDeposit = deposits[_id];
+        // require(myDeposit, "ERR__INVALID_DEPOSIT_ID");
+        require(
+            block.timestamp >= myDeposit.unlockTimeStamp,
+            "ERR__TOO_EARLY"
+        );
+        require(msg.sender == myDeposit.receiver, "ERR__INCORRECT_RECEIVER");
+        require(!myDeposit.withdrawn, "ERR_ALREADY_WITHDRAWN");
 
-        require(block.timestamp >= vault[_id].unlockTimeStamp, "Tokens are still locked!");
-        require(msg.sender == vault[_id].withdrawer, "Not the withdrawer!");
-        require(vault[_id].deposited, "Tokens are not yet deposited!");
-        require(!vault[_id].withdrawn, "Tokens are already withdrawn!");
-
-        vault[_id].withdrawn = true;
-
-        userTokenBalance[msg.sender][address(vault[_id].token)] = userTokenBalance[msg.sender][address(vault[_id].token)].sub(vault[_id].amount);
-
-        vault[_id].token.transfer(msg.sender, vault[_id].amount);
-
-        emit tokenWithdrawalComplete(msg.sender, vault[_id].amount);
+        myDeposit.withdrawn = true;
+        myDeposit.token.transfer(msg.sender, myDeposit.amount);
+        emit TokenWithdrawal(_id);
     }
 
-    function getDepositsByWithdrawer(address _withdrawer) view external returns (uint256[] memory) {
-        return depositsByWithdrawers[_withdrawer];
+    function getDepositsByReceiver(address _receiver)
+        external
+        view
+        returns (Deposit[] memory)
+    {
+        Deposit[] memory results = new Deposit[](
+            receiverDepositCount[_receiver]
+        );
+        uint256 counter = 0;
+
+        for (uint256 i = 0; i < deposits.length; i++) {
+            if (deposits[i].receiver == _receiver) {
+                results[counter] = deposits[i];
+                counter++;
+            }
+        }
+        return results;
     }
 
-    function getDepositsByTokenAddress(address _id) view external returns (uint256[] memory) {
-        return depositsByTokenAddress[_id];
-    }
+    function getDepositsBySender(address _sender)
+        external
+        view
+        returns (Deposit[] memory)
+    {
+        Deposit[] memory results = new Deposit[](senderDepositCount[_sender]);
+        uint256 counter = 0;
 
+        for (uint256 i = 0; i < deposits.length; i++) {
+            if (deposits[i].sender == _sender) {
+                results[counter] = deposits[i];
+                counter++;
+            }
+        }
+
+        return results;
+    }
 }
